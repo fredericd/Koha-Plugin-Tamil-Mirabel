64753e1f83d240a60f6b153aa04602689e66ca01
Frédéric Demians <f.demians@tamil.fr>
2019-10-29 08:16:31 +0100

    Init dépôt

---
+++ Koha/Plugin/Tamil/Mirabel.pm
@@ -0,0 +1,486 @@
+package Koha::Plugin::Tamil::Mirabel;
+
+use Modern::Perl;
+use utf8;
+use base qw(Koha::Plugins::Base);
+use CGI qw(-utf8);
+use C4::Context;
+use C4::Biblio;
+use Koha::Cache;
+use Mojo::UserAgent;
+use Mojo::JSON qw(decode_json encode_json);
+use Template;
+
+
+
+## Here is our metadata, some keys are required, some are optional
+our $metadata = {
+    name            => 'Koha ⇄ Mir@bel',
+    description     => 'Interfaçage entre Koha et Mir@bel',
+    author          => 'Tamil s.a.r.l.',
+    date_authored   => '2019-10-20',
+    date_updated    => "2019-10-20",
+    minimum_version => '18.11.00.000',
+    maximum_version => undef,
+    copyright       => '2019',
+    version         => '1.0.0',
+};
+
+
+my $DEFAULT_LISTE_TEMPLATE = <<EOS;
+<div class="alert alert-success" role="alert">
+ <h4 class="alert-heading">Accès en ligne</h4>
+ <p>
+  Voici la liste des revues détenues à la Bibliothèque, pour lesquels un accès
+  en-ligne est disponible.
+ </p>
+</div>
+<div>
+[% FOREACH t IN titres %]
+ <h3>
+  [% t.titre %]
+  <a href="/cgi-bin/koha/opac-detail.pl?biblionumber=[% t.identifiantpartenaire %]">
+   <img src="/opac-tmpl/bootstrap/images/favicon.ico" title="Voir cette revue dans ce Catalogue"/>
+  </a>
+  <a href="https://reseau-mirabel.info/revue/[% t.revueid + %]" target="_blank">
+   <img src="https://reseau-mirabel.info/images/favicon.ico" width="16px" title="Dans Mir\@bel"/>
+  </a>
+  <a href="[% t.url %]" target="_blank" title="Chez son éditeur">
+   <span style="background-color: #a0a0a0; padding: 2px;  color:white; font-size:11px;">É</span>
+  </a>
+ </h3>
+ <p>
+  [% t.editeurs.join(' • ') %]
+  ; [% t.periodicite %]
+  ;
+  [% FOREACH i IN t.issns %]
+   [% i.issn %] ([% i.suport %])[% ", " UNLESS loop.last %]
+  [% END %]
+   ;
+  [% t.datedebut %] - [% t.datefin %]
+ </p>
+ <ul>
+  [% FOREACH a IN t.acces %]
+   <li>
+    <a href="[% a.url %]">[% a.ressource %]</a>,
+    [% a.numerodebut %]-[% a.numerofin %],
+    [% a.datedebut %]-[% a.datefin %],
+    [% a.contenu %] ([% a.diffusion %])
+   </li>
+  [% END %]
+ </ul>
+[% END %]
+</div>
+EOS
+
+my $DEFAULT_BIBLIO_TEMPLATE = <<EOS;
+<div id="mirabel" aria-labelledby="ui-id-7" class="ui-tabs-panel ui-widget-content ui-corner-bottom" role="tabpanel" aria-expanded="false" aria-hidden="true" style="display:block; border: 1px solid #c0c0c0; padding: 7px; margin-bottom:0px; margin-top: 5px;">
+ <div class="content_set">
+  <table class="table table-sm table-condensed table-hover" style="margin-bottom: 0px;">
+   <thead class="thead-dark">
+    <th>
+     <a href="https://reseau-mirabel.info">
+      <img src="https://reseau-mirabel.info/images/favicon.ico" width="16px" title="Mir\@bel"/>
+     </a> Accès en-ligne
+    </th>
+    <th>Ressource</th>
+    <th>Accès</th>
+    <th>Numéros</th>
+   </thead>
+   <tbody>
+    [% FOREACH a IN acces %]
+     <tr>
+      <td><a href="[% a.url %]">[% a.contenu %]</a></td>
+      <td><a href="https://reseau-mirabel.info/ressource/[% a.ressourceid %]">[% a.ressource %]</td>
+      <td>[% a.diffusion %]</td>
+      <td>
+       [% a.datedebut %]
+       [% IF a.numerodebut %]([% a.numerodebut %])[% END %]
+       —
+       [% IF a.datefin != "" %]
+         [% a.datefin %]
+         [% IF a.numerofin %]([% a.numerofin %])[% END %]
+       [% ELSE %]
+         ...
+       [% END %]
+      <td>
+     <tr>
+    [% END %]
+   </tbody>
+  </table>
+ </div>
+</div>
+EOS
+
+
+sub new {
+    my ($class, $args) = @_;
+
+    $args->{metadata} = $metadata;
+    $args->{metadata}->{class} = $class;
+    $args->{cache} = Koha::Cache->new();
+
+    $class->SUPER::new($args);
+}
+
+
+sub config {
+    my $self = shift;
+
+    my $c = $self->{args}->{c};
+    unless ($c) {
+        $c = $self->retrieve_data('c');
+        if ($c) {
+            utf8::encode($c);
+            $c = decode_json($c);
+        }
+        else {
+            $c = {};
+        }
+    }
+    $c->{url} ||= {};
+    $c->{url}->{web} ||= 'https://reseau-mirabel.info';
+    $c->{url}->{api} ||= 'https://reseau-mirabel.info/api';
+    $c->{url}->{timeout} ||= 600;
+    $c->{opac}->{liste}->{template} ||= $DEFAULT_LISTE_TEMPLATE;
+    $c->{opac}->{biblio}->{template} ||= $DEFAULT_BIBLIO_TEMPLATE;
+    $c->{metadata} = $self->{metadata};
+
+    $self->{args}->{c} = $c;
+
+    return $c;
+}
+
+
+sub get_form_config {
+    my $cgi = shift;
+    my $c = {
+        url => {
+            web     => undef,
+            api     => undef,
+            timeout => undef,
+        },
+        partenaire => {
+            id  => undef,
+            cle => undef,
+        },
+        opac => {
+            liste => {
+                template => undef,
+            },
+            biblio => {
+                active => undef,
+                affiche => {
+                    ou => undef,
+                    id => undef,
+                },
+                cherche => undef,
+                template => undef,
+            }
+        }
+    };
+
+    my $set;
+    $set = sub {
+        my ($node, $path) = @_;
+        return if ref($node) ne 'HASH';
+        for my $subkey ( keys %$node ) {
+            my $key = $path ? "$path.$subkey" : $subkey;
+            my $subnode = $node->{$subkey};
+            if ( ref($subnode) eq 'HASH' ) {
+                $set->($subnode, $key);
+            }
+            else {
+                $node->{$subkey} = $cgi->param($key);
+            }
+        }
+    };
+
+    $set->($c);
+    return $c;
+}
+
+
+sub configure {
+    my ($self, $args) = @_;
+    my $cgi = $self->{'cgi'};
+
+    if ( $cgi->param('save') ) {
+        my $c = get_form_config($cgi);
+        $self->store_data({ c => encode_json($c) });
+        print $self->{'cgi'}->redirect(
+            "/cgi-bin/koha/plugins/run.pl?class=Koha::Plugin::Tamil::Mirabel&method=tool");
+    }
+    else {
+        my $template = $self->get_template({ file => 'configure.tt' });
+        $template->param( c => $self->config() );
+        $self->output_html( $template->output() );
+    }
+}
+
+
+sub install() {
+    my ($self, $args) = @_;
+}
+
+
+sub upgrade {
+    my ($self, $args) = @_;
+
+    my $dt = DateTime->now();
+    $self->store_data( { last_upgraded => $dt->ymd('-') . ' ' . $dt->hms(':') } );
+
+    return 1;
+}
+
+
+sub uninstall() {
+    my ($self, $args) = @_;
+}
+
+
+sub ws() {
+    my ($self, $service, $param) = @_;
+
+    my $c   = $self->config();
+    my $api = $c->{url}->{api};
+    my $cle = $c->{partenaire}->{cle};
+    my $ua  = $self->{ua} ||= Mojo::UserAgent->new;
+    my $url = $api . $service;
+    $param ||= {};
+    if ( $param->{partenaire} && $param->{partenaire} eq 'delete' ) {
+        delete $param->{partenaire};
+    }
+    else {
+        $param->{partenaire} = $cle;
+    }
+    $url .= '?' . join('&', map { "$_=" . $param->{$_} } keys %$param)
+      if keys %$param;
+
+    my $res = $ua->get($url)->result;
+    return $res ? $res->json : undef;
+}
+
+
+sub get_titres {
+    my $self = shift;
+
+    my $titres = $self->{cache}->get_from_cache('titres');
+    unless ($titres) {
+        if ( $titres = $self->ws('/mes/titres') ) {
+            for my $titre (@$titres) {
+                my $id = $titre->{revueid};
+                $titre->{acces} = $self->ws('/acces/revue', {revueid => $id});
+            }
+            my $c = $self->config();
+            $self->{cache}->set_in_cache(
+                'titres', $titres, { expiry => $c->{url}->{timeout} });
+        }
+    }
+    return $titres;
+}
+
+
+sub html_titres {
+    my $self = shift;
+
+    if ( my $titres = $self->get_titres() ) {
+        my $c = $self->config();
+        my $template = Template->new();
+        my $text = $c->{opac}->{liste}->{template};
+        my $html = '';
+        $template->process(\$text, { titres => $titres }, \$html)
+            or die "Mauvais template liste : " . $template->error();
+        return $html;
+    }     
+}
+
+
+sub get_all_acces {
+    my $self = shift;
+
+    my $acces = $self->{cache}->get_from_cache('acces');
+    unless ($acces) {
+        if ( $acces = $self->ws('/mes/acces') ) {
+            $acces = [ sort { $a->{ressource} cmp $b->{ressource} } @$acces ];
+            my %titre = map { $_->{titreid} => {} } @$acces;
+            for my $id ( keys %titre ) {
+                $titre{$id} = $self->ws("/titres/$id", {partenaire => 'delete'});
+            }
+            for (@$acces) {
+                $_->{titre} = $titre{ $_->{titreid} };
+            }
+            my $c = $self->config();
+            $self->{cache}->set_in_cache(
+                'acces', $acces, { expiry => $c->{url}->{timeout} });
+        }
+    }
+    return $acces;
+}
+
+
+sub get_acces {
+    my ($self, $id) = @_;
+
+    my $key = "acces_$id";
+    my $acces = $self->{cache}->get_from_cache($key);
+    unless ($acces) {
+        if ( $acces = $self->ws('/acces/titres', {issn => $id}) ) {
+            $acces = [ sort { $a->{ressource} cmp $b->{ressource} } @$acces ];
+            my $c   = $self->config();
+            $self->{cache}->set_in_cache(
+                $key, $acces, { expiry => $c->{url}->{timeout} })
+        }
+    }
+    return $acces;
+}
+
+
+sub html_acces {
+    my ($self, $id) = @_;
+
+    if ( my $acces = $self->get_acces($id) ) {
+        my $c = $self->config();
+        my $template = Template->new();
+        my $text = $c->{opac}->{biblio}->{template};
+        my $html = '';
+        $template->process(\$text, { acces => $acces }, \$html)
+            or die "Mauvais template biblio : " . $template->error();
+        return $html;
+    }     
+}
+
+
+sub tool {
+    my ($self, $args) = @_;
+
+    my $cgi = $self->{'cgi'};
+
+    my $template;
+    my $tool = $cgi->param('tool');
+    if ( $tool ) {
+        if ($tool eq 'acces') {
+            $template = $self->get_template({ file => 'acces.tt' });
+            $template->param( acces => $self->get_all_acces() );
+        }
+        elsif ($tool eq 'titres') {
+            $template = $self->get_template({ file => 'titres.tt' });
+            $template->param( titres => $self->get_titres() );
+        }
+    }
+    else {
+        $template = $self->get_template({ file => 'home.tt' });
+    }
+    $template->param( c => $self->config() );
+    $template->param( TOOL => $tool ) if $tool;
+    $self->output_html( $template->output() );
+}
+
+
+sub intranet_js {
+    my $self = shift;
+
+    q|
+      <script>
+        $(document).ready(function(){
+          if ( $('body').is("#tools_tools-home") ) {
+            $('.container-fluid .col-sm-4:nth-child(2) dl').append(
+             '<dt><a href="/cgi-bin/koha/plugins/run.pl?class=Koha::Plugin::Tamil::Mirabel&method=tool">Koha ⇄ Mir@bel</a></dt>' +
+             '<dd>Interfaçage de Mir@bel avec Koha</dd>');
+          }
+          $('#toplevelmenu li:last-child ul').append(
+            '<li>' +
+            '<a href="/cgi-bin/koha/plugins/run.pl?class=Koha::Plugin::Tamil::Mirabel&method=tool">Koha ⇄ Mir@bel</a>' +
+            '</li>'
+          );
+        });
+      </script>
+    |;
+}
+
+
+sub opac_js {
+    my $self = shift;
+
+    my $url = $ENV{REQUEST_URI};
+    return if $url !~ /opac-detail\.pl|opac-main\.pl/;
+
+    return $url =~ /opac-detail/ ? $self->opac_detail()
+                                 : $self->opac_liste();
+}
+
+
+sub opac_detail {
+    my $self = shift;
+    my $c = $self->config();
+
+    return if $c->{opac}->{biblio}->{active} == 0;
+
+    # On ne fait rien si on n'est pas sur la page de détail, sur une notice
+    # qui a un ISSN
+    my $url = $ENV{REQUEST_URI};
+    return unless $url =~ /biblionumber=([0-9]+)/;
+    my $biblionumber = $1;
+    my $sth = C4::Context::dbh->prepare("
+        SELECT issn FROM biblioitems WHERE biblionumber = ?");
+    $sth->execute($biblionumber);
+    my ($issn) = $sth->fetchrow;
+    return unless $issn;
+
+    my $acces = $self->html_acces($issn);
+    $acces = encode_json($acces);
+    utf8::decode($acces);
+    my $biblio = $c->{opac}->{biblio};
+    my $location = $biblio->{affiche}->{ou};
+    my $id       = $location eq 'div' ? $biblio->{affiche}->{id} :
+                   $location eq 'ex_dessus' ? '#catalogue_detail_biblio' : 0;
+
+    return <<EOS;
+<script>
+\$(document).ready(function(){
+  function mirabelAcces(location,html) {
+      if (location !== '0') {
+        \$(location).append(html);
+      } else {
+        var tabMenu = "<li class='ui-state-default ui-corner-top' role='tab' tabindex='-1' aria-controls='mirabel' aria-labelledby='ui-id-7' aria-selected='false'><a href='#mirabel' class='ui-tabs-anchor' role='presentation' tabindex='-1' id='ui-id-7'>Mir\@bel</a></li>";
+        var tabs = \$('#bibliodescriptions').tabs();
+        var ul = tabs.find("ul");
+        \$(ul).append(tabMenu);
+        \$(tabs).append(html);
+        tabs.tabs("refresh");
+      }
+  }
+
+  mirabelAcces('$id', $acces);
+
+});
+</script>
+EOS
+
+}
+
+
+sub opac_liste {
+    my $self = shift;
+
+    my $url = $ENV{REQUEST_URI};
+    return unless $url =~ /mirabel=liste/;
+
+    my $titres = $self->html_titres();
+    $titres = encode_json($titres);
+    utf8::decode($titres);
+
+    return <<EOS;
+<script>
+\$(document).ready(function(){
+  var id = \$('.main .span7').hide();
+  id.html($titres);
+  id.show();
+  \$('.breadcrumb a').attr('href', '/');
+});
+</script>
+EOS
+
+}
+
+1;
---
+++ README.md
@@ -0,0 +1,234 @@
+# Extension Koha ⇄ Mir@bel
+
+**Koha ⇄ Mir@bel** est un plugin de Koha qui étend les fonctionnalités de Koha en y intégrant les accès en- ligne aux revues repérées dans Mir@bel.
+
+[Mir@bel](https://reseau-mirabel.info) est une **base de connaissances mutualisée des accès en-ligne aux revues en sciences humaines francophones**. Les conditions d'accès aux revues peuvent être libre, restreintes ou sous abonnement. Il y a des accès au texte intégral des revues, aux sommaires des numéros, à l'indexation des articles ou à leur résumé. La période de publication de la revue couverte par chaque accès, ainsi que les lacunes et les conditions d'accès sont également disponibles. Mir@bel est géré par un réseau de **partenaires**. Ce sont les bibliothèques et centres de documentation qui sont autorisés à mettre à jour Mir@bel et à récupérer localement les informations de Mir@bel au moyen de services web.
+
+L'extension Koha ⇄ Mir@bal permet à un Partenaire de Mir@bel qui gère son Catalogue de bibliothèque dans le SIGB Koha de faire remonter dans son OPAC Koha les informations de la base de connaissances Mir@bel. Ces échanges sont réalisés au moyens des [Service Web](https://reseau-mirabel.info/api) de Mir@bel.
+
+Les informations Mir@bel apparaissent à l'OPAC à deux endroits :
+
+1. **Liste des revues** — Une URL de l'OPAC affiche la liste de toutes les revues du partenaire avec pour chaque revue ses points d'accès signalés dans Mir@bel.
+2. **Liste des accès** — Dans la page de détail d'une notice bibliographique d'un périodique tous ses accès sont affichés.
+
+## Installation
+
+**Activation des plugins** — Si ce n'est pas déjà fait, dans Koha, activez les plugins. Demandez à votre prestataire Koha de le faire, ou bien vérifiez les points suivants :
+
+* Dans `koha-conf.xml`, activez les plugins.
+* Dans le fichier de configuration d'Apache, définissez l'alias `/plugins`. 
+* Activez la préférence système `UseKohaPlugins`. 
+
+**▼ TÉLÉCHARGEMENT ▼** — Récupérez sur le site [Tamil](http://www.tamil.fr) l'archive de l'Extension **[Koha ⇄ Mir@bel](http://www.tamil.fr/download/koha-plugin-tamil-mirabel-1.0.0.kpz)**.
+
+**Installation** — Dans l'interface pro de Koha, allez dans Outils > Outils de Plugins. Cliquez sur Télécharger un plugin. Choisissez l'archive téléchargée à l'étape précédente. Cliquez sur Télécharger.
+
+**Configuration** — Dans les Outils de plugins, vous voyez l'Extension *Koha ⇄ Mir@bel*. Cliquez sur Actions > Configurer. 
+
+## Utilisation de l'extension
+
+### Liste des revues
+
+La liste des revues gérées dans Koha pour lesquelles des accès en-ligne sont proposés via Mir@bel est accessible à cette URL :
+
+```
+<OPAC_KOHA>/cgi-bin/koha/opac-main.pl?mirabel=liste
+```
+
+Créez un lien vers cette URL sur la page d'Accueil de votre OPAC. Par exemple :
+
+```html
+<a href="/cgi-bin/koha/opac-main.pl?mirabel=liste">Nos revues accessibles en-ligne</a>
+```
+
+Le formatage de la liste des revues est assuré par un template d'affichage. Un template est fourni par défaut, que vous pouvez modifier. Le système de traitement de template est le même que celui utilisé dans Koha : [Template Toolkit](http://www.template-toolkit.org).
+
+L'extension Mir@el envoie au template une variable appelée `titres` qui est un tableau de titres de revues, avec pour chacune un tableau de ses accès. Par exemple :
+
+```json
+[ 
+   { 
+      "acces":[ 
+         { 
+            "contenu":"Intégral",
+            "datedebut":"1956",
+            "datefin":"2009",
+            "diffusion":"restreint",
+            "id":9674,
+            "nodebut":1,
+            "numerodebut":"Vol. 1, no 1",
+            "ressource":"SAGE journals online",
+            "ressourceid":253,
+            "titreid":1012,
+            "url":"http:\/\/journals.sagepub.com\/loi\/asj",
+            "voldebut":1
+         },
+         { 
+            "contenu":"Sommaire",
+            "datedebut":"1993-01",
+            "datefin":"2019-09",
+            "diffusion":"libre",
+            "id":11134,
+            "mailurl":"https:\/\/signal.sciencespo-lyon.fr\/alerte\/revue?revueId=392",
+            "nodebut":1,
+            "nofin":3,
+            "numerodebut":"Vol. 36, no 1",
+            "numerofin":"Vol. 62, no 3",
+            "ressource":"Sign@l",
+            "ressourceid":296,
+            "rssurl":"https:\/\/signal.sciencespo-lyon.fr\/rss\/rss.php?flux=signal-sommaires-revue&param=392",
+            "titreid":1012,
+            "url":"https:\/\/signal.sciencespo-lyon.fr\/revue\/392",
+            "voldebut":36,
+            "volfin":62
+         },
+         { 
+            "contenu":"Intégral",
+            "datedebut":"1955-01-01",
+            "datefin":"2015-11-01",
+            "diffusion":"restreint",
+            "embargoinfo":"P4Y",
+            "id":14119,
+            "nodebut":1,
+            "nofin":4,
+            "numerodebut":"Vol. 1, no 1",
+            "numerofin":"Vol. 58, no 4",
+            "ressource":"JSTOR",
+            "ressourceid":143,
+            "titreid":1012,
+            "url":"https:\/\/www.jstor.org\/journal\/actasoci",
+            "voldebut":1,
+            "volfin":58
+         },
+         { 
+            "contenu":"Intégral",
+            "datedebut":"1975-01-03",
+            "datefin":"2002-12-31",
+            "diffusion":"restreint",
+            "id":19178,
+            "ressource":"EBSCOhost - Business Source Complete",
+            "ressourceid":238,
+            "titreid":1012,
+            "url":"http:\/\/search.ebscohost.com\/direct.asp?db=bth&jid=ACT&scope=site"
+         }
+      ],
+      "bouquetpartenaire":null,
+      "datedebut":"1955",
+      "datefin":null,
+      "editeurs":[ 
+         "Scandinavian University Press"
+      ],
+      "id":1012,
+      "identifiantpartenaire":"81793",
+      "issns":[ 
+         { 
+            "issn":"0001-6993",
+            "issnl":"0001-6993",
+            "statut":"valide",
+            "sudocnoholding":false,
+            "sudocppn":"038657856",
+            "suport":"papier",
+            "worldcatocn":"471992889"
+         },
+         { 
+            "issn":"1502-3869",
+            "issnl":"0001-6993",
+            "statut":"valide",
+            "sudocnoholding":false,
+            "sudocppn":"058349782",
+            "suport":"electronique",
+            "worldcatocn":"56628041"
+         }
+      ],
+      "langues":[ 
+         "eng"
+      ],
+      "obsoletepar":null,
+      "periodicite":"trimestriel",
+      "prefixe":null,
+      "revueid":978,
+      "titre":"Acta Sociologica : Journal of the Scandinavian Sociological Association",
+      "url":"http:\/\/asj.sagepub.com\/",
+      "url_revue_mirabel":"https:\/\/reseau-mirabel.info\/revue\/978"
+   },
+   {
+       etc.
+   }
+```
+
+
+
+### Liste des accès
+
+Dans Koha, sur la page de détail d'une notice de périodique, les accès fournis par Mir@bel peuvent être affichés. Cette affichage est pilotée par la section _Affichage avec les notices à l'OPAC_ de la configuration :
+
+* **Activer** pour activer/désactiver l'affichage.
+* **Emplacement** pour sélectionner l'emplacement où afficher les accès en-ligne :
+  * **Dans le pavé biblio** pour afficher les accès au-milieu de la notice, à un emplacement repéré par un identifiant html, par exemple : `.publisher`
+  * **Entre le pavé biblio et les exemplaires**
+  * **Dans un nouvel onglet du pavé des exemplaires**
+* **Recherche par** pour spécifier la clé de recherche de la revue dans Mir@bel : l'ISSN, le biblionumber Koha ou le PPN Sudoc. Pour le moment, seul l'ISSN est pris en compte.
+* **Template** — L'affichage est déterminé par un _template_ fourni par défaut. Vous pouvez le modifier.
+
+L'extension Mir@bel envoie au template la variable `acces` qui contient un tableau des accès à la revue. Par exemple :
+
+```json
+[ 
+   { 
+      "contenu":"Résumé",
+      "datedebut":"2003-12",
+      "diffusion":"libre",
+      "id":7978,
+      "nodebut":150,
+      "numerodebut":"no 150",
+      "ressource":"Actes de la recherche en sciences sociales",
+      "ressourceid":868,
+      "rssurl":"http:\/\/www.arss.fr\/feed\/",
+      "titreid":28,
+      "url":"http:\/\/www.arss.fr\/sommaires\/"
+   },
+   { 
+      "contenu":"Intégral",
+      "datedebut":"2001",
+      "datefin":"2016",
+      "dateinfo":"Barrière mobile de 2 ans",
+      "diffusion":"libre",
+      "id":4916,
+      "nodebut":136,
+      "numerodebut":"no 136",
+      "ressource":"Cairn.info",
+      "ressourceid":3,
+      "titreid":28,
+      "url":"http:\/\/www.cairn.info\/revue-actes-de-la-recherche-en-sciences-sociales.htm"
+   },
+   { 
+      "contenu":"Sommaire",
+      "datedebut":"1975",
+      "datefin":"2019-09",
+      "diffusion":"libre",
+      "id":4588,
+      "mailurl":"https:\/\/signal.sciencespo-lyon.fr\/alerte\/revue?revueId=26",
+      "nodebut":1,
+      "nofin":229,
+      "numerodebut":"Vol. 1, no 1",
+      "numerofin":"no 229",
+      "ressource":"Sign@l",
+      "ressourceid":296,
+      "rssurl":"https:\/\/signal.sciencespo-lyon.fr\/rss\/rss.php?flux=signal-sommaires-revue&param=26",
+      "titreid":28,
+      "url":"https:\/\/signal.sciencespo-lyon.fr\/revue\/26",
+      "voldebut":1
+   }
+]
+```
+
+## VERSION
+
+* **1.0.0** / nov. 2019 — Version initiale
+
+## LICENCE
+
+This software is copyright (c) 2019 by Tamil s.a.r.l..
+
+This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
+
