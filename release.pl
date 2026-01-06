437907e9c77371be515dda878a4ccad7d5bcc3d3
Frédéric Demians <f.demians@tamil.fr>
2026-01-06 10:19:37 +0000

    Mineur

--- release.pl
+++ release.pl
@@ -7,6 +7,6 @@ use Koha::Plugin::Tamil::Mirabel;
 my $metadata = $Koha::Plugin::Tamil::Mirabel::metadata;
 my $file = $metadata->{canonicalname} . '-' . $metadata->{version} . '.kpz';
 system "zip -r $file ./Koha";
-system "scp README.md st16:tamil-gatsby/src/doc/mirabel.md";
-system "scp $file st16:download/";
+say "Copier README.md vers wordpress:/var/www/tamil/src/doc/mirabel.md";
+say "Copier $file vers wordpress:/var/www/download/";
 say "Lancer la mise à jour tamil.fr : gatsby build";
